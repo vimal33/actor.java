53c47e3144b02a93a18c9c6e83b142028a6a1397
Vimal Raj .C <118258607+vimal33@users.noreply.github.com>
2022-12-04 23:35:09 +0530

    Add files via upload

---
+++ actor/src/actor/Actor.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package actor;
+
+/**
+ *
+ * @author Vimal
+ */
+public class Actor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     childclass obj=new childclass();
+     obj.acting();
+     obj.dancing();
+
+
+//TODO code application logic here
+    }
+    
+}
---
+++ actor/src/actor/childclass.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package actor;
+
+/**
+ *
+ * @author Vimal
+ */
+public class childclass {
+    String name;
+    String dob;
+    String occupation;
+    void acting() {
+        System.out.println("he acts very well");
+    }
+            void dancing() {
+                System.out.println("he dances very well");
+            }
+    
+    
+}
